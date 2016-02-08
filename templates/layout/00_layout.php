28d4bb7113a611584c7027065fa0aa5198556e8b
Francesco Montefoschi <francesco.monte@gmail.com>
2016-02-08 15:47:05 +0000

    Google Analytics

--- templates/layout/00_layout.php
+++ templates/layout/00_layout.php
@@ -26,6 +26,8 @@
     <!--[if lt IE 9]>
     <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
+    <meta name="google-site-verification" content="GlKlGkbxuWQ3LNWx0akLYkqoWLrtF5b0ug9UpGadSuc" />
+    <meta name="google-site-verification" content="vL1Qo6JWKZBq6fzVJLJhbyqHya1TBhZd78OJDA-Qe1s" />
 </head>
 <body class="<?= $params['html']['float'] ? 'with-float' : ''; ?>">
     <?= $this->section('content'); ?>
@@ -53,5 +55,17 @@
     } ?>
 
     <script src="<?= $base_url; ?>themes/daux/js/daux.js"></script>
+	<script type="text/javascript">
+	 var _gaq = _gaq || [];
+	 _gaq.push(['_setAccount', 'UA-40057783-1']);
+	 _gaq.push(['_setDomainName', 'udoo.org']);
+	 _gaq.push(['_setAllowLinker', true]);
+	 _gaq.push(['_trackPageview']);
+	 (function() {
+	   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	   ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+	   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	 })();
+	</script>
 </body>
 </html>

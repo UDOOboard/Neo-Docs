8dd2e9917ef23d61a42cbeb30e45117f9f431f2d
Francesco Montefoschi <francesco.monte@gmail.com>
2016-02-11 11:56:56 +0100

    Website auto update

--- deploy.sh
+++ deploy.sh
@@ -28,3 +28,7 @@ echo "Deploying"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
 #git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages 
 
+sleep 10
+
+echo "Updating UDOO.org"
+curl -A "Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3" "http://www.udoo.org/wp-content/plugins/neo-docs/update.php?repo=docs-neo&t=$TKX"

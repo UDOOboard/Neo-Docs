3f2e4cce2c86a2ba38a73127a89306f06c5ce866
Francesco Montefoschi <francesco.monte@gmail.com>
2017-03-01 11:52:14 +0100

    Fixed header

--- themes/daux/templates/partials/navbar_content.php
+++ themes/daux/templates/partials/navbar_content.php
@@ -1,6 +1,6 @@
 <a class="udoologo pull-left" href="<?= $params['base_page'] . $params['index']->getUri(); ?>">
-		<img src="http://www.udoo.org/docs/img/logo_docs.png"> <span>Docs</span>
-		<a class="btn btn-lg button_docs pull-right" href="http://www.udoo.org/docs-neo/">UDOO Neo Docs</a>
+		<img src="http://www.udoo.org/docs-neo/img/logo_docs_neo.png"> <span>Docs</span>
+		<a class="btn btn-lg button_docs pull-right" href="http://www.udoo.org/docs/">UDOO Quad/Dual Docs</a>
 </a>
 
 <?php if ($params['html']['search']) {
